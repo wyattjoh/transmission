8d05082e9faca00c6827ab49b9fd2231fa54a805
Wyatt Johnson <wyattjoh@gmail.com>
2015-03-08 20:34:19 -0600

    Added foreground requirement

--- transmission.sh
+++ transmission.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec /sbin/setuser nobody transmission-daemon -g /config  -e /config/transmission.log
+exec /sbin/setuser nobody transmission-daemon -f -g /config -e /config/transmission.log
